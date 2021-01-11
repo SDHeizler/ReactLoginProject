8ebdf7d89c69ae38d0331bc29a6180b420dd52d6
Scott Heizler <scottdheizler91@gmail.com>
2021-01-11 17:57:16 -0500

    Added Not found route and changed route paths

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import User from './Components/UserPage';
+import NotFound from './Components/404';
 import './App.css';
 
 class App extends React.Component {
@@ -14,9 +15,10 @@ class App extends React.Component {
       <GlobalStateProvider>
         <Switch>
           <Route exact path='/ReactLoginProject' ><Home></Home></Route>
-          <Route path='/Register' ><Register></Register></Route>
-          <Route exact path='/Login' ><Login></Login></Route>
-          <Route path='/Login/User' ><User></User></Route>
+          <Route path='/ReactLoginProject/Register' ><Register></Register></Route>
+          <Route exact path='/ReactLoginProject/Login' ><Login></Login></Route>
+          <Route path='/ReactLoginProject/Login/User' ><User></User></Route>
+          <Route path=''><NotFound></NotFound></Route>
         </Switch>
       </GlobalStateProvider>
     </div>
